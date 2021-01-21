ee9f5735fca24ed05c86deeb4aadccf4913fe5df
Juliusz Brzeczkowski <X00145602@mytudublin.ie>
2021-01-21 11:45:41 +0000

    Add project files.

---
+++ BankingApplication/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using BankingApplication.ViewModels;
+using BankingApplication.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BankingApplication
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
