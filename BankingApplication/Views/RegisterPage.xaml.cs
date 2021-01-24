6a12b93064de1a2518d6654ad4c42bccb0d55f11
Juliusz Brzeczkowski <X00145602@mytudublin.ie>
2021-01-24 20:49:37 +0000

    Created Login page, Main page with tabbed layout, Register page. Deleted mock data store

---
+++ BankingApplication/Views/RegisterPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BankingApplication.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasBackButton(this, false);
+        }
+
+        private void Button_Clicked_Register(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
