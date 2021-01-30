4b8bd632baacf3d1dda7f356894b2ce378c2f0c1
Juliusz Brzeczkowski <X00145602@mytudublin.ie>
2021-01-30 23:16:48 +0000

    Created new active branch

---
+++ BankingApplication/Models/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using System.Text;
+using SQLite;
+
+namespace BankingApplication.Models
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string fName { get; set; }
+        public string sName { get; set; }
+        public string phoneNo { get; set; }
+        public string password { get; set; }
+
+        public static MobileServiceClient client = new MobileServiceClient("");
+
+        public async Task<bool> savePerson()
+        {
+            try
+            {
+                await client.GetTable<Person>().InsertAsync(this);
+                return true;
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                var response = await msioe.Response.Content.ReadAsStringAsync();
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static async Task<List<Person>> ReadPerson()
+        {
+            return await client.GetTable<Person>().ToListAsync();
+        }
+
+
+
+
+
+
+
+    }
+}
