e6a47384d7fcde516551c51cebe46387f3c92490
Juliusz Brzeczkowski <X00145602@mytudublin.ie>
2021-03-12 18:47:13 +0000

    Better Version with this error "Response status code does not indicate success: 400 (Bad Request)."

--- BankingApplication/Models/Person.cs
+++ BankingApplication/Models/Person.cs
@@ -21,15 +21,32 @@ namespace BankingApplication.Models
 
         //public static MobileServiceClient client = new MobileServiceClient("HTTP connection goes here :)");
 
-        private static readonly HttpClient httpClient = new HttpClient();
+        //private static readonly HttpClient httpClient = new HttpClient();
         private static readonly string baseURI = "https://10.0.2.2:44380/api/";
 
-        
+        // This method must be in a class in a platform project, even if
+        // the HttpClient object is constructed in a shared project.
+        public static HttpClientHandler GetInsecureHandler()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+            {
+                if (cert.Issuer.Equals("CN=localhost"))
+                    return true;
+                return errors == System.Net.Security.SslPolicyErrors.None;
+            };
+            return handler;
+        }
+#if DEBUG
+       static HttpClientHandler insecureHandler = GetInsecureHandler();
+        private static readonly HttpClient httpClient = new HttpClient(insecureHandler);
+#else
+   HttpClient httpClient = new HttpClient();
+#endif
 
-             
-       
+
         //POST method
-       public static async Task RunPost(Person personToAdd)
+        public static async Task RunPost(Person personToAdd)
         {
             try
             {
